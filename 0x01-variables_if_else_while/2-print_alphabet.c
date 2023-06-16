cd10272a690793442209d19dfc1626067dddf33f
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-06-16 15:59:14 +0000

    Add and fix some tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Main function
+* Description: Main function
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 97; n < 123; ++n)
+		putchar(n);
+	putchar(10);
+	return (0);
+}
