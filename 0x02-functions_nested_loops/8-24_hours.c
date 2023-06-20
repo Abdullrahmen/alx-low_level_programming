1179cb684481c117752e41cfd2fd2ed2f814ad98
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-06-20 14:33:36 +0000

    Add and fix some tasks

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* jack_bauer - prints every minute of the day, starting from 00:00 to 23:59
+*/
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 3; ++i)
+		for (j = 0; j < 4; ++j)
+			for (k = 0; k < 6; ++k)
+				for (l = 0; l < 10; ++l)
+				{
+					_putchar(i + 48);
+					_putchar(j + 48);
+					_putchar(':');
+					_putchar(k + 48);
+					_putchar(l + 48);
+					_putchar('\n');
+				}
+}
+
