6a26531b7ca06785c71a53eb8280b3c1c782d4fe
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-06-20 17:24:39 +0000

    Add and fix some tasks

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,16 +1,4 @@
 #include <stdio.h>
-/**
-* get_first_digit - gets first digit of n
-* @n: the number
-* Return: the first digit
-*/
-int get_first_digit(unsigned long int n)
-{
-	while (n >= 10)
-		n %= 10;
-	return (n);
-}
-
 
 /**
 * main - prints the first 50 Fibonacci numbers
@@ -18,30 +6,25 @@ int get_first_digit(unsigned long int n)
 */
 int main(void)
 {
-	unsigned long int last_1 = 0, last_2 = 0;
-	unsigned long int addition = 0;
+	unsigned long int last_2,last_1;
+	unsigned long int tmp;
 	unsigned int size = 0;
-
 
 	last_1 = 1;
 	last_2 = 2;
 	size += 2;
 	printf("1, 2, ");
-
 	while (size < 98)
 	{
-		addition = last_1 + last_2;
-		if (get_first_digit(addition) == 1 || get_first_digit(addition) == 2)
-		{
-			printf("%ld", addition);
+		tmp = last_1 + last_2;
+		printf("%ld", tmp);
 
-
-			if (size != 97)
-				printf(", ");
-			++size;
-		}
 		last_1 = last_2;
-		last_2 = addition;
+		last_2 = tmp;
+
+		if (size != 97)
+			printf(", ");
+		++size;
 	}
 	printf("\n");
 	return (0);
