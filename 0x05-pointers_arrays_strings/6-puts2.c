d92d83683b2174067a91957c579ef214a13b70db
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-06-26 20:40:53 +0000

    Add and fix some tasks

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string.
+ *@str: the string to use.
+ */
+void puts2(char *str)
+{
+	int i, length;
+
+	for (length = 0; str[length] != '\0'; ++length)
+
+	for (i = 0; i < length; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
