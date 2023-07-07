a4e02c157c0cd8e04dfa98bd92dce50b71e7f2dc
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-07-07 05:20:45 +0000

    Add and fix some tasks

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+/**
+* _islower - checks for lowercase character
+* @c: The character that will be checked
+* Return: 1 if c is lowercase, 0 otherwise
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
+
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+/**
+* _abs - computes the absolute value of an intege
+* @n: The number that will get absolute of it
+* Return: The absolute of n
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (n * -1);
+}
+
