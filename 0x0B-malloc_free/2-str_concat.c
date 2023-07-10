7081fb0963af1cb43c7348b7d30596f1b3fbb2ce
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-07-10 05:11:19 +0000

    Add and fix some tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,33 +12,43 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	int i = 0, len = 0;
 
-	while (s1[i])
-		++i;
-	len += i;
+	if (s1 != NULL)
+	{
+		while (s1[i])
+			++i;
+		len += i;
+	}
 
-	i = 0;
-	while (s2[i])
-		++i;
-	len += i + 1;
+	if (s2 != NULL)
+	{
+		i = 0;
+		while (s2[i])
+			++i;
+		len += i + 1;
+	}
 
 	s3 = malloc(sizeof(*s3) * len);
 	if (s3 == NULL)
 		return (NULL);
 
 	i = 0;
-	while (s1[i])
-	{
-		s3[i] = s1[i];
-		++i;
-	}
-	len = i;
+	if (s1 != NULL)
+		while (s1[i])
+		{
+			s3[i] = s1[i];
+			++i;
+		}
+	
 
+	len = i;
 	i = 0;
-	while (s2[i])
-	{
-		s3[len + i] = s2[i];
-		++i;
-	}
+	if (s2 != NULL)
+		while (s2[i])
+		{
+			s3[len + i] = s2[i];
+			++i;
+		}
+
 	return (s3);
 }
 
