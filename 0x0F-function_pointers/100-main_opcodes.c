8b3864dc30352b8db36b890d8cc1d353675f56f5
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-07-20 01:15:26 +0000

    Add and fix some tasks

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -30,9 +30,14 @@ int main(int argc, char *argv[])
 	arr = (char *)main;
 
 	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
 		printf("%02hhx ", arr[i]);
-
-	printf("\n");
+	}
 	return (0);
 }
 
