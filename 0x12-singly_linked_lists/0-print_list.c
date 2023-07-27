f336dd3a60f70aafef98a952c0dff1ab78be6105
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-07-27 21:35:57 +0000

    Add and fix some tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,22 +9,12 @@
  */
 size_t print_list(const list_t *h)
 {
-	unsigned int n_nodes = 0;
-	list_t const *l = 0;
+	size_t n = 0;
 
-	if (!h)
-		return (n_nodes);
-
-	l = h;
-	++n_nodes;
-	do {
-		if (!l->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		l = l->next;
-		++n_nodes;
-	} while (l);
-	return (n_nodes);
+	while (h)
+	{
+		h = h->next;
+		++n;
+	}
+	return (n);
 }
-
