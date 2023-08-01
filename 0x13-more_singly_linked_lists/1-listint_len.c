b8453b48b7ad7198cdbc676472512be0ce22b07c
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-08-01 21:35:13 +0000

    Add and fix some tasks

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t counter = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		++counter;
+		h = h->next;
+	}
+	return (counter);
+}
+
